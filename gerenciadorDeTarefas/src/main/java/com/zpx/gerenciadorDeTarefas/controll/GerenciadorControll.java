1919f018f65623be9eeb1b3123ec8ebbf5449ab9
Edgar Jesus <mendesbr92@gmail.com>
2023-11-04 19:08:35 -0300

    configurado o swagger e criado uma doc simples

--- gerenciadorDeTarefas/src/main/java/com/zpx/gerenciadorDeTarefas/controll/GerenciadorControll.java
+++ gerenciadorDeTarefas/src/main/java/com/zpx/gerenciadorDeTarefas/controll/GerenciadorControll.java
@@ -2,6 +2,8 @@ package com.zpx.gerenciadorDeTarefas.controll;
 
 import com.zpx.gerenciadorDeTarefas.domain.Tarefas;
 import com.zpx.gerenciadorDeTarefas.service.TarefasService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,6 +11,8 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/tarefas")
+@Api(value = "Gerenciador de Tarefas")
+@CrossOrigin(origins = "*")
 public class GerenciadorControll {
 
     private final TarefasService service;
@@ -19,21 +23,25 @@ public class GerenciadorControll {
     }
 
     @PostMapping
+    @ApiOperation(value = "Cria um item na lista de tarefas")
     public void create(@RequestBody Tarefas tarefas){
         service.create(tarefas);
     }
 
     @GetMapping
+    @ApiOperation(value = " lista as tarefas")
     public List<Tarefas> list (){
         return service.list();
     }
 
     @PutMapping
+    @ApiOperation(value = "atualiza as tarefas")
     public Tarefas update (@RequestBody Tarefas tarefas){
         return service.update(tarefas);
     }
 
     @DeleteMapping
+    @ApiOperation(value = "apaga uma tarefa")
     public void delete(@RequestBody Tarefas tarefas){
         service.delete(tarefas);
     }
